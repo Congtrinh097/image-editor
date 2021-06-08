ed30f9c3ca6c0e3cc95fc9c286d210b6d6cd53af
trinhtc <congtrinh097@gmail.com>
2021-06-08 21:17:26 +0700

    Update image editor

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" >
+    <div class="image-edit-container">
+        <div class="image-editor"> 
+          <image-editor
+              :include-ui="true"
+              :options="options"
+            >
+          </image-editor>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+// Load Style Code
+import "tui-color-picker/dist/tui-color-picker.css";
+import "tui-image-editor/dist/tui-image-editor.css";
+import {ImageEditor} from '@toast-ui/vue-image-editor';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ImageEditor
+  },
+  data() {
+    return {
+        options: {
+          includeUI: {
+            loadImage: {
+              path: "/assets/SampleWhite.png",
+              name: "EditImage"
+            },
+            initMenu: "draw"
+          },
+          usageStatistics: false,
+          cssMaxWidth: 1000,
+          cssMaxHeight: 2000
+        }
+    }
+  },
+  methods:{
+     async done() {
+      let imageData = this.$refs.tuiImageEditor.editorInstance.toDataURL();
+      console.log(imageData)
+    },
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 98vh;
+}
+.image-edit-container {
+  width: 100%;
+  height: 100%;
+}
+.image-editor {
+  height: 100%;
+  position: relative;
+}
+.tui-image-editor-header-logo {
+  display: none;
 }
 </style>
